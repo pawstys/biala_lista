9771e2eebb4d04b0100dca79936d72a58f934a6a
Pawel Stys <stys.pawel@gmail.com>
2023-01-05 22:36:35 +0100

    Dodatno automatyczne wczytywanie plikow plaskich

--- biala_lista.py
+++ biala_lista.py
@@ -1,6 +1,7 @@
 import json, sqlite3
 from hashlib import sha512
 from datetime import datetime
+from os import sep, listdir
 
 def ObliczSkrot(dt, nip, nrb, iter):
     klucz = datetime.strftime(dt,'%Y%m%d') + nip + nrb
@@ -57,31 +58,42 @@ def UtworzBazeDanych():
     cursor.execute("CREATE UNIQUE INDEX IF NOT EXISTS indeks_zwolnieni on SkrotyPodatnikowZwolnionych (Data, Klucz)")
     cursor.execute("CREATE UNIQUE INDEX IF NOT EXISTS indeks_maski on Maski (Data, Maska)")
 
-def WczytajPlikPlaski():
-    cursor.execute(f"SELECT Data FROM Naglowek WHERE Data = '{data}'")
+def ObliczSumeKontrolna(plik_json):
+    with open(f'.{sep}pliki_json{sep}{plik_json}','rb') as f:
+        dane = f.read()
+        return sha512(dane).hexdigest()
+
+def WczytajPlikPlaski(dt):
+    cursor.execute(f"SELECT Data FROM Naglowek WHERE Data = '{dt}'")
     if cursor.fetchone() == None:
-        print(f'Wczytuję plik z dnia: {data}')
-        path = f".\pliki_json\\{data.strftime('%Y%m%d')}.json"
-        with open(path):
-            dane = json.load(open(path))
-        cursor.execute(f"INSERT INTO Naglowek VALUES ('{data}', {dane['naglowek']['liczbaTransformacji']})")     
-        cursor.executemany("INSERT INTO SkrotyPodatnikowCzynnych VALUES (?, ?)", [(data, dane['skrotyPodatnikowCzynnych'][i]) for i in range(len(dane['skrotyPodatnikowCzynnych']))])
-        cursor.executemany("INSERT INTO SkrotyPodatnikowZwolnionych VALUES (?, ?)", [(data, dane['skrotyPodatnikowZwolnionych'][i]) for i in range(len(dane['skrotyPodatnikowZwolnionych']))])
-        cursor.executemany("INSERT INTO Maski VALUES (?, ?)", [(data, dane['maski'][i]) for i in range(len(dane['maski']))])
+        plik_json = f"{dt.strftime('%Y%m%d')}.json"
+        path = f'.{sep}pliki_json{sep}{plik_json}'
+        with open(f'.{sep}pliki_json{sep}{plik_json}.sha512sum') as f:
+            wpis = f.readline()
+            if (wpis[-14:-1] == plik_json) and (wpis[:128] == ObliczSumeKontrolna(plik_json)):
+                print(f'Plik {plik_json} jest poprawny. Wczytuję plik z dnia: {dt}')
+                with open(path):
+                    dane = json.load(open(path))
+                cursor.execute(f"INSERT INTO Naglowek VALUES ('{dt}', {dane['naglowek']['liczbaTransformacji']})")     
+                cursor.executemany("INSERT INTO SkrotyPodatnikowCzynnych VALUES (?, ?)", [(dt, dane['skrotyPodatnikowCzynnych'][i]) for i in range(len(dane['skrotyPodatnikowCzynnych']))])
+                cursor.executemany("INSERT INTO SkrotyPodatnikowZwolnionych VALUES (?, ?)", [(dt, dane['skrotyPodatnikowZwolnionych'][i]) for i in range(len(dane['skrotyPodatnikowZwolnionych']))])
+                cursor.executemany("INSERT INTO Maski VALUES (?, ?)", [(dt, dane['maski'][i]) for i in range(len(dane['maski']))])
+            else:
+                print(f'Niezgodna suma kontrolna w pliku {plik_json}.sha512sum, plik {plik_json} nie zostanie zaczytany')
     else:
-        print(f'Plik z dnia {data} został już zaczytany')
+        print(f'Plik z dnia {dt} został już zaczytany')
 
 
 if __name__ == '__main__':
 
     db = sqlite3.connect('biala_lista.db')
     cursor = db.cursor()
-
-    for i in range(1,6):
-       data = datetime(2023,1,i).date()    
-       UtworzBazeDanych()
-       WczytajPlikPlaski()
-
+    UtworzBazeDanych()
+    for plik in listdir(f'.{sep}pliki_json'):
+        if len(plik) == 13:
+            if plik[-5:] == '.json':
+                data = datetime.strptime(plik[:8],'%Y%m%d').date()
+                WczytajPlikPlaski(data)
     db.commit()
 
     print(WeryfikujPodatnik(datetime(2023,1,1).date(), '5250000794', '55124069601701080001214133'))
